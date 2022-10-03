01ed2ce5bcf0cee3f328d7c8e70e93a14311393d
Raj Singh <rksinghj4@gmail.com>
2022-10-03 12:08:36 +0530

    Added Constructor and Field injection

---
+++ ConstructorAndFieldInjection/app/src/main/java/com/example/constructorandfieldinjection/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.constructorandfieldinjection
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+/**
+ * If you annotate an Android class with @AndroidEntryPoint,
+ * then you also must annotate Android classes that depend on it(MainActivity).
+ */
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+    //Once dependent activity's super.onCreate() will be called
+    // required dependencies will be injected
+    @Inject
+    lateinit var userRepository: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        userRepository.saveUser("test@abc.com", "1111", "MainFragment")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
---
+++ ConstructorAndFieldInjection/app/src/main/java/com/example/constructorandfieldinjection/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.example.constructorandfieldinjection
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton //Now only single object will be used whereever we inject it.
+class UserRepository @Inject constructor(val loggerService: LoggerService){
+    fun saveUser(email: String, password: String, from: String) {
+        loggerService.log("From $from User saved in DB")
+    }
+}
